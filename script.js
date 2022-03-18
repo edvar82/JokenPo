42844ed0421d8576ced2328dd6473ab19920cffc
Edvar <fodose123@gmail.com>
2022-03-18 19:04:44 -0300

    att

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ function jogar() {
     } else {
         //Lógica do computador
         var sorteio = Math.floor(Math.random() * 3);
+        let vit = 0;
+        let emp = 0;
+        let der = 0;
 
         switch (sorteio) {
             case 0:
@@ -35,15 +38,20 @@ function jogar() {
         if ((papel.checked == true && sorteio == 1) || (pedra.checked == true && sorteio == 0) || (tesoura.checked == true && sorteio == 2)) {
             placar.style.color = "black";
             placar.innerHTML = "EMPATE!";
+            emp++;
+            document.querySelector("#empate").innerHTML = `Empate = ${emp}`;
         } else if ((papel.checked == true && sorteio == 0) || (pedra.checked == true && sorteio == 2) || (tesoura.checked == true && sorteio == 0)) {
             placar.style.color = "green";
             placar.innerHTML = "JOGADOR VENCEU!";
             acertou();
-
+            vit += 1;
+            document.querySelector("#vitoria").innerHTML = `Vitória = ${vit}`;
         } else {
             placar.style.color = "red";
             placar.innerHTML = "COMPUTADOR VENCEU!";
             errou();
+            der += 1;;
+            document.querySelector("#derrota").innerHTML = `Derrota = ${der}`;
         }
     }
 }
